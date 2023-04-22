a0e613c63b4c1babf104b983d1b01f4aed81b8db
BuddeRasteruRowan <125320831+BuddeRasteruRowan@users.noreply.github.com>
2023-04-22 17:54:03 -0400

    Update OrganizationBox.java
    Sorts alphabetically by organization name

--- src/finalProject/OrganizationBox.java
+++ src/finalProject/OrganizationBox.java
@@ -1,5 +1,7 @@
 package finalProject;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import javafx.geometry.Insets;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
@@ -16,15 +18,27 @@ public class OrganizationBox {
         Label organizations = new Label("Your oragnizations");
         styleHeader(organizations);
         vbox.getChildren().add(organizations);
+        ArrayList<String> sortedKeys = new ArrayList();
         for(Organization organization : person.getOrganizationsAndRoles().keySet()){
-            Hyperlink hyperlink = new Hyperlink(organization.getName());
-            hyperlink.setOnAction(e -> {
-                //function to go to organization
-            });
-            styleLink(hyperlink);
-            Label label = new Label(person.getOrganizationsAndRoles().get(organization).getClass().getSimpleName());
-            styleLabel(label);
-            vbox.getChildren().addAll(hyperlink, label);
+        sortedKeys.add(organization.getName());
+        }
+        Collections.sort(sortedKeys, String.CASE_INSENSITIVE_ORDER);
+        while(sortedKeys.size() != 0){
+            for(Organization organization : person.getOrganizationsAndRoles().keySet()){
+                if(sortedKeys.size() != 0)
+                    if(organization.getName() == sortedKeys.get(0)){
+                        Hyperlink hyperlink = new Hyperlink(organization.getName());
+                        hyperlink.setOnAction(e -> {
+                            //function when clicked
+                        });
+                        styleLink(hyperlink);
+                        Label label = new Label(person.getOrganizationsAndRoles().get(organization).getClass().getSimpleName());
+                        styleLabel(label);
+                        vbox.getChildren().addAll(hyperlink, label);
+                        System.out.println(sortedKeys.get(0));
+                        sortedKeys.remove(0);
+                    }
+            }
         }
     }
     public void styleLink(Hyperlink hyperlink){
